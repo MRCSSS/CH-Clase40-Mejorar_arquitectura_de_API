1ed083275f66f775277520a1053642201656b0cb
MRCSSS <marcossolis49@gmail.com>
2023-01-01 22:16:45 -0600

    Adding change

--- src/apiV1/users/controller.js
+++ src/apiV1/users/controller.js
@@ -1,52 +1,19 @@
 /* =================================== MODULES =================================== */
-import * as service from "./service.js";
-import * as dto from "./dto.js";
 import * as userService from './service.js';
+import * as userDTO from "./dto.js";
 import logger from '../../utils/logger.js';
 /* ================================== INSTANCES ================================== */
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, './public/images');
-//     },
-//     filename: (req, file, cb) => {
-//         cb(null, `${Date.now()}_${file.originalname}`);
-//     }
-// });
-// const upload = multer({storage: storage});
+
 /* ================================== FUNCTIONS ================================== */
+
 /* ================================= MIDDLEWARES ================================= */
-    /* ----------------------- Passport ------------------------ */
-// passport.use(new LocalStrategy(
-//     async function(username, password, done) {
-//         const user = await usersDao.searchUser(username);
 
-//         if (user === null) {
-//             return done(null, false);
-//         } else {
-//             const match = await bcrypt.compare(password, user.password);
-
-//             if(!match){
-//                 return done(null, false);
-//             }
-//             return done(null, user);
-//         }
-//     }
-// ));
-// passport.serializeUser((user, done)=>{
-//     done(null, user.username);
-// });
-// passport.deserializeUser( async (username, done)=>{
-//     const user = await usersDao.searchUser(username);
-//     done(null, user);
-// });
-// siteOper.use(passport.initialize());
-// siteOper.use(passport.session());
 
 /* ================================= CONTROLLERS ================================= */
 export async function register(req,res) {
     const data = await service.getRoot(req.body);
     try {
-        await cartService.createCart();
+        await userService.createCart();
         logger.info(`status: 200, route: '${req.method} ${req.baseUrl} ${req.url}'`);
         return res.status(201);
     } catch (error) {
